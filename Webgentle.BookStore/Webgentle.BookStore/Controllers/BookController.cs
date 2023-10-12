b7983e7e0cce9a46d73242fa9fcc2ed667b7544d
Abhishekinfor <abhishek86364@gmail.com>
2023-10-12 17:30:36 +0530

    Added libman to use client side library.

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repositary;
+
+namespace Webgentle.BookStore.Properties.Controllers
+{
+        public class BookController : Controller
+        {
+
+        private readonly BookRepositary _bookRepositary = null;
+
+        public BookController()
+        {
+            _bookRepositary = new BookRepositary();
+        }
+            public ViewResult GetAllBooks()
+            {
+                //var data = _bookRepositary.GetAllBooks();
+                  return View();
+            }
+
+            public BookModel GetBookByID(int id)
+            {
+                return _bookRepositary.GetABookByID(id);
+            }
+
+            public List<BookModel> SearchBooks(string BookName, string AuthorName)
+            {
+                return _bookRepositary.SearchBooks(BookName,AuthorName);
+            }
+
+        }
+    }
---
+++ Webgentle.BookStore/Webgentle.BookStore/Repositary/BookRepositary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+
+namespace Webgentle.BookStore.Repositary
+{
+    public class BookRepositary
+    {
+        public List<BookModel> GetAllBooks() {
+            return DataSource();
+        }
+
+        public BookModel GetABookByID(int id)
+        {
+            return DataSource().Where(x => x.ID == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBooks(string BookName, string AuthorName)
+        {
+            return DataSource().Where(x => x.Title == BookName && x.Author == AuthorName).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+              new BookModel(){ID=1, Title="MVC", Author="Nitish"},
+              new BookModel(){ID=2, Title="MVC", Author="John"},
+              new BookModel(){ID=3, Title="C#", Author="Monika"},
+              new BookModel(){ID=4, Title="Java", Author="Webgentle"},
+              new BookModel(){ID=5, Title="PHP", Author="Webgentle"},
+            };
+            
+        }
+    }
+}
